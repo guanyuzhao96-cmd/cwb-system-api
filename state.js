e80c9b4f38695800d79a35387a62db4f42b8007d
guanyuzhao96-cmd <guanyuzhao96@gmail.com>
2026-09-16 13:28:21 +0800

    Publish CWB system API plugin: state.js

---
+++ state.js
@@ -0,0 +1,9 @@
+export const MODULE_NAME = 'cwb_system_api';
+
+export const state = {
+    chatId: 'unknown_chat_init',
+    messages: [],
+    updating: false,
+    batchRunning: false,
+    batchStopRequested: false,
+};
